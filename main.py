4c0c2e2e9f59bea92499239540262482da5282ca
cthompson71 <40264316+Cameron-Thompson@users.noreply.github.com>
2023-06-24 21:22:30 +0100

    looping through json response

--- main.py
+++ main.py
@@ -1,49 +1,35 @@
 import requests 
 import json
 import xml
-import pymysql
+import mysql
 
-fileType = ''
+forecastDict = ''
 
 #get the overall daily predictions for the next days, exclude current , minutely and hourly from the response
 api_url = "https://api.openweathermap.org/data/3.0/onecall?lat=54.5973&lon=5.9301&exclude=minutely,hourly,current&appid=cf6e18bcfba8040fd3d0bef6a12975af"
 response = requests.get(api_url)
 
 try:
-   if (response.headers.get('content_type').startswith('application/json')):
-        fileType = 'JSON'
+   if (response.headers.get('Content-Type').startswith('application/json')):
+        forecastDict = response.json()
    else:
         raise Exception(TypeError('unsupported format, please use JSON only')) 
 except TypeError as te:
      print("Error: " + te)
 
 
-def  getLatitude(JSON ) {
+for x in forecastDict['daily']:
+     print("printing x")
+     print (x)
 
-}
+     tempDict = {
+          "latitude": forecastDict['lat'],
+          "longitude": forecastDict['lon'],
+          "timestamp": x['dt'],
+          "temperature": x['temp']['day'],
+          "windspeed": x['wind_speed'],
+          "wind_deg": x['wind_deg'],
+          "precipitation_chance": x['pop'],
+     }
+     print(tempDict)
 
-def getLongitude() {
-
-}
-
-
-def getUTCTime() {
-
-}
-
-
-def getTemperature() {
-
-}
-
-def getWindSpeed() {
-
-}
-
-def getWindDirection() {
-
-}
-
-def getPrecipitationChance() {
-
-}
