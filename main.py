cda6b4ec0faa06a5d2c2e69b38324c128e7f8641
cthompson71 <40264316+Cameron-Thompson@users.noreply.github.com>
2023-06-24 17:53:11 +0100

    Working api call

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests 
+
+api_url = "https://api.openweathermap.org/data/3.0/onecall?lat=54.5973&lon=5.9301&appid=cf6e18bcfba8040fd3d0bef6a12975af"
+response = requests.get(api_url)
+print (response.json())
+
+
+
+
+
