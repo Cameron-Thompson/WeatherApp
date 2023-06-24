d7de70745155b8e3a9f8521968ff7cc4908793db
cthompson71 <40264316+Cameron-Thompson@users.noreply.github.com>
2023-06-24 22:07:05 +0100

    refactor methods

---
+++ helpers.py
@@ -0,0 +1,24 @@
+def writeToDictionary(forecastDict, records):
+    for dailyRecord in forecastDict['daily']:
+         tempDict = {
+          #write a temporary dictionary and then append to an array
+          "latitude": forecastDict['lat'],
+          "longitude": forecastDict['lon'],
+          "timestamp": dailyRecord['dt'],
+          "temperature": dailyRecord['temp']['day'],
+          "windspeed": dailyRecord['wind_speed'],
+          "wind_deg": dailyRecord['wind_deg'],
+          "precipitation_chance": dailyRecord['pop'],
+     }
+         records.append(tempDict)
+
+
+def validateResponse(response):
+    try:
+       if (response.headers.get('Content-Type').startswith('application/json')):
+            forecastDict = response.json()
+       else:
+            raise Exception(TypeError('unsupported format, please use JSON only')) 
+    except TypeError as te:
+         print("Error: " + te)
+    return forecastDict
--- main.py
+++ main.py
@@ -2,34 +2,19 @@ import requests
 import json
 import xml
 import mysql
+from helpers import validateResponse, writeToDictionary
 
 forecastDict = ''
 
 #get the overall daily predictions for the next days, exclude current , minutely and hourly from the response
 api_url = "https://api.openweathermap.org/data/3.0/onecall?lat=54.5973&lon=5.9301&exclude=minutely,hourly,current&appid=cf6e18bcfba8040fd3d0bef6a12975af"
 response = requests.get(api_url)
-
-try:
-   if (response.headers.get('Content-Type').startswith('application/json')):
-        forecastDict = response.json()
-   else:
-        raise Exception(TypeError('unsupported format, please use JSON only')) 
-except TypeError as te:
-     print("Error: " + te)
+forecastDict = validateResponse(response)
 
 
-for x in forecastDict['daily']:
-     print("printing x")
-     print (x)
+records = []
+writeToDictionary(forecastDict, records)
 
-     tempDict = {
-          "latitude": forecastDict['lat'],
-          "longitude": forecastDict['lon'],
-          "timestamp": x['dt'],
-          "temperature": x['temp']['day'],
-          "windspeed": x['wind_speed'],
-          "wind_deg": x['wind_deg'],
-          "precipitation_chance": x['pop'],
-     }
-     print(tempDict)
+#push records to the db 
+
 
